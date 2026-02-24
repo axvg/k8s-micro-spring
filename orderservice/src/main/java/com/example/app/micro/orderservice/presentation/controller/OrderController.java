dafbd4a79336b3606ab44a7b4125d300765c10d9
Alex Vega <axvg@users.noreply.github.com>
2026-02-23 19:33:03 -0500

    add presentation

---
+++ orderservice/src/main/java/com/example/app/micro/orderservice/presentation/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.example.app.micro.orderservice.presentation.controller;
+
+import com.example.app.micro.orderservice.application.dto.OrderRequest;
+import com.example.app.micro.orderservice.application.dto.OrderResponse;
+import com.example.app.micro.orderservice.application.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderRequest request) {
+        OrderResponse response = orderService.createOrder(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getOrderById(id));
+    }
+}
